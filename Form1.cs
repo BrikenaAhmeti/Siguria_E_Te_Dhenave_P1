27cb4f4382baba12eebee144659c264150e69361
besnikkelmendi <35761060+besnikkelmendi@users.noreply.github.com>
2018-05-10 19:10:11 +0200

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,4 @@
+
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -23,3 +24,21 @@ namespace DESready
         }
   
         DES des = new DES();
+
+private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtKey.Text.Length <= 8)
+            {
+                txtCipher.Text = des.Enkripto(txtPlain.Text, txtKey.Text);
+            }
+            else
+            {
+                MessageBox.Show("Qelesi duhet te jete makssimumi 8 karaktere");
+                txtKey.Focus();
+            }
+        }
