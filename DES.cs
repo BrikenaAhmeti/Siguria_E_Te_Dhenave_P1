55e3b4cd2fc9ccd577eda623c532c76ac454e9aa
besnikkelmendi <35761060+besnikkelmendi@users.noreply.github.com>
2018-05-10 18:23:01 +0200

    Update DES.cs

--- DES.cs
+++ DES.cs
@@ -1,3 +1,4 @@
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,3 +23,8 @@ namespace DESready
                                         41,52,31,37,47,55,30,40,51,45,33,48,
                                         44,49,39,56,34,53,46,42,50,36,29,32 };
         
+        //Permutacioni per zgjerim  
+        private int[] zgj = new int[] { 32,1,2,3,4,5,4,5,6,7,8,9,
+                                        8,9,10,11,12,13,12,13,14,15,16,17,
+                                        16,17,18,19,20,21,20,21,22,23,24,25,
+                                        24,25,26,27,28,29,28,29,30,31,32,1 };
